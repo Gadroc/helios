dd088278f4a810b3a6ed7ffb51829c6ad3f13bc1
BlueFinBima <neil.g.a.larmour@gmail.com>
2018-06-01 08:01:36 +0100

    Created a new subclass Cabin Pressure gauge

---
+++ Helios/Gauges/AV-8B/Cabin Pressure/CabinPressure.cs
@@ -0,0 +1,32 @@
+﻿//  Copyright 2014 Craig Courtney
+//    
+//  Helios is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Helios is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace GadrocsWorkshop.Helios.Gauges.AV8B.cabinPressure
+{ 
+    using GadrocsWorkshop.Helios.ComponentModel;
+    using GadrocsWorkshop.Helios.Gauges.A_10.CabinPressure;
+    using System;
+    using System.Windows;
+
+    [HeliosControl("Helios.AV8B.cabinPressure", "AV-8B Cabin Pressure", "AV-8B Gauges", typeof(GaugeRenderer))]
+    public class cabinPressure : CabinPressure
+    {
+        public cabinPressure()
+            : base()
+        {
+            Components.RemoveAt(Components.Count-1);
+        }
+    }
+}
