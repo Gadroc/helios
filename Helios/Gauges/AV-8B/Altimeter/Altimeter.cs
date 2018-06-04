03eebd067d9c315fdcfd92a267dc6bcc67846ae4
BlueFinBima <neil.g.a.larmour@gmail.com>
2018-06-04 09:44:05 +0100

    AV-8B Altimeter corrections and first drop of the FA-18C interface.

--- Helios/Gauges/AV-8B/Altimeter/Altimeter.cs
+++ Helios/Gauges/AV-8B/Altimeter/Altimeter.cs
@@ -34,25 +34,25 @@ namespace GadrocsWorkshop.Helios.Gauges.AV8B.ADI
         public Altimeter()
             : base("Altimeter", new Size(364, 376))
         {
-            Components.Add(new GaugeImage("{Helios}/Gauges/AV-8B/Altimeter/altimeter_backplate.xaml", new Rect(32d, 38d, 300d, 300d)));
+            Components.Add(new GaugeImage("{Helios}/Gauges/A-10/Altimeter/altimeter_backplate.xaml", new Rect(32d, 38d, 300d, 300d)));
 
-            _tensDrum = new GaugeDrumCounter("{Helios}/Gauges/AV-8B/Altimeter/alt_drum_tape.xaml", new Point(71d, 164d), "#", new Size(10d, 15d), new Size(30d, 45d));
+            _tensDrum = new GaugeDrumCounter("{Helios}/Gauges/A-10/Altimeter/alt_drum_tape.xaml", new Point(71d, 164d), "#", new Size(10d, 15d), new Size(30d, 45d));
             _tensDrum.Clip = new RectangleGeometry(new Rect(71d, 144d, 150d, 81d));
             Components.Add(_tensDrum);
 
-            _drum = new GaugeDrumCounter("{Helios}/Gauges/AV-8B/Common/drum_tape.xaml", new Point(101d, 164d), "#%00", new Size(10d, 15d), new Size(30d, 45d));
+            _drum = new GaugeDrumCounter("{Helios}/Gauges/A-10/Common/drum_tape.xaml", new Point(101d, 164d), "#%00", new Size(10d, 15d), new Size(30d, 45d));
             _drum.Clip = new RectangleGeometry(new Rect(101d, 144d, 150d, 81d));
             Components.Add(_drum);
 
-            _airPressureDrum = new GaugeDrumCounter("{Helios}/Gauges/AV-8B/Common/drum_tape.xaml", new Point(214d, 233d), "###%", new Size(10d, 15d), new Size(15d, 20d));
+            _airPressureDrum = new GaugeDrumCounter("{Helios}/Gauges/A-10/Common/drum_tape.xaml", new Point(214d, 233d), "###%", new Size(10d, 15d), new Size(15d, 20d));
             _airPressureDrum.Value = 2992d;
             _airPressureDrum.Clip = new RectangleGeometry(new Rect(214d, 233d, 60d, 20d));
             Components.Add(_airPressureDrum);
 
-            Components.Add(new GaugeImage("{Helios}/Gauges/AV-8B/Altimeter/altimeter_faceplate.xaml", new Rect(32d, 38d, 300d, 300d)));
+            Components.Add(new GaugeImage("{Helios}/Gauges/A-10/Altimeter/altimeter_faceplate.xaml", new Rect(32d, 38d, 300d, 300d)));
 
             _needleCalibration = new CalibrationPointCollectionDouble(0d, 0d, 1000d, 360d);
-            _needle = new GaugeNeedle("{Helios}/Gauges/AV-8B/Altimeter/altimeter_needle.xaml", new Point(182d, 188d), new Size(16d, 257d), new Point(8d, 138.5d));
+            _needle = new GaugeNeedle("{Helios}/Gauges/A-10/Altimeter/altimeter_needle.xaml", new Point(182d, 188d), new Size(16d, 257d), new Point(8d, 138.5d));
             Components.Add(_needle);
 
             //Components.Add(new GaugeImage("{Helios}/Gauges/A-10/Common/gauge_bezel.png", new Rect(0d, 0d, 364d, 376d)));
