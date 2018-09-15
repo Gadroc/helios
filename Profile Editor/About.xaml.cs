3070d8203eddf5933d07dbcaee17906cc8ec526b
BlueFinBima <neil.g.a.larmour@gmail.com>
2018-09-15 12:22:06 +0100

    About Screen Changes for Profile Editor

--- Profile Editor/About.xaml.cs
+++ Profile Editor/About.xaml.cs
@@ -33,6 +33,8 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
         protected override void OnActivated(EventArgs e)
         {
             VersionBlock.Text = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            ContributionBlock.Text = ContributionBlock.Text + " Cylution yzfanimal damien022";
+            StatusBlock.Text = "Beta 3";
             base.OnActivated(e);
         }
 
