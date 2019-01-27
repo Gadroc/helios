84366da0bca9830b3d0828ea9bc0cb131f9b4112
damien022 <damien.charveriat@gmail.com>
2019-01-27 18:11:58 +0000

    Adding IFEI panel and buttons

--- Helios/Gauges/FA-18C/IFEI/IFEI.cs
+++ Helios/Gauges/FA-18C/IFEI/IFEI.cs
@@ -30,17 +30,64 @@ namespace GadrocsWorkshop.Helios.Gauges.FA18C
         private Rect _scaledScreenRect = SCREEN_RECT;
 
         private string _aircraft;
-        private String _font = "MS 33558";
-        //private String _font = "Franklin Gothic";
+        private String _font = "Seven Segement";
+        private Color _textColor = Color.FromRgb(12, 12, 12);
+        private Color _backGroundColor = Color.FromRgb(12, 100, 12);
+        private string _imageLocation = "{Helios}/Gauges/FA-18C/IFEI/";
+        private bool _useBackGround = true;
+
         public IFEI_FA18C()
-            : base("IFEI", new Size(458, 400))
+            : base("IFEI", new Size(779, 702))
         {
+            double spacing = 70;
+            double start = 64;
+            AddButton("MODE", 402, start, new Size(87, 62));
+            AddButton("QTY", 402, start + spacing, new Size(87, 62));
+            AddButton("UP", 402, start + 2 * spacing, new Size(87, 62));
+            AddButton("DOWN", 402, start + 3 * spacing, new Size(87, 62));
+            AddButton("ZONE", 402, start + 4 * spacing, new Size(87, 62));
+            AddButton("ET", 402, start + 5 * spacing, new Size(87, 62));
  
         }
 
         public override string BezelImage
         {
-            get { return "{Helios}/Gauges/FA-18C/IFEI/IFEI.png"; }
+            get { return _imageLocation + "IFEI.png"; }
+        }
+
+        private void AddTextDisplay(string name, double x, double y, Size size, double baseFontsize, string testDisp, bool hTextAlignedRight)
+        {
+            TextDisplay display = AddTextDisplay(
+                name: name,
+                pos: new Point(x, y),
+                size: size,
+                font: "Hornet_UFC",
+                baseFontsize: baseFontsize,
+                horizontalAlignment: TextHorizontalAlignment.Left,
+                verticalAligment: TextVerticalAlignment.Top,
+                testTextDisplay: testDisp,
+                textColor: Color.FromArgb(0xff, 0x40, 0xb3, 0x29),
+                backgroundColor: Color.FromArgb(0xff, 0x00, 0x00, 0x00),
+                useBackground: true
+                );
+
+            if (hTextAlignedRight)
+            {
+                display.TextFormat.HorizontalAlignment = TextHorizontalAlignment.Right;
+            }
+        }
+
+        private void AddButton(string name, double x, double y, Size size)
+        {
+            Point pos = new Point(x, y);
+            AddButton(
+                name: name,
+                posn: pos,
+                size: size,
+                image: _imageLocation + "IFEI_" + name + ".png",
+                pushedImage: _imageLocation + "IFEI_" + name + "DN.png",
+                buttonText: ""
+                );
         }
 
         public override bool HitTest(Point location)
