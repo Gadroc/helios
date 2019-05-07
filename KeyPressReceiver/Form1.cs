62d4ee74d98113a8bffd4e10410062470a9c7311
Piet <kiwi.lost.in.melb@gmail.com>
2019-05-07 12:59:33 +1000

    Testing update

--- KeyPressReceiver/Form1.cs
+++ KeyPressReceiver/Form1.cs
@@ -12,136 +12,174 @@ using System.Runtime.InteropServices;
 
 namespace KeyPressReceiver
 {
-  public partial class Form1 : Form
-  {
-    Boolean DontClose = true;
-    private delegate void SafeCallDelegate(string text);
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        Boolean DontClose = true;
+        private delegate void SafeCallDelegate(string text);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
+            tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Visible = true;
+            this.BringToFront();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ((e.CloseReason == CloseReason.UserClosing) && (DontClose))
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+        private void OnTCPStatusChanged(string dataIn)
+        {
+            if (this.DontClose == false) { return; }
+            //Handle multi-threading
+            if (InvokeRequired)
+            {
+                Invoke(new SafeCallDelegate(OnTCPStatusChanged), new object[] { dataIn });
+                return;
+            }
+            TCPClient TCPClient = TCPClient.Instance;
+            if (TCPClient.ClientConnected)
+            {
+                timer1.Interval = 60000; // 60 seconds still connected test
+                lbStatus.Text = "Connected";
+                lbStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                timer1.Interval = 10000; // 10 second test to try to connect
+                lbStatus.Text = "Disconnected";
+                lbStatus.ForeColor = Color.Red;
+            }
+        }
+
+
+        private void OnTCPDataReceived(string dataIn)
+        {
+            if (this.DontClose == false) { return; }
+            //Handle multi-threading
+            if (InvokeRequired)
+            {
+                Invoke(new SafeCallDelegate(OnTCPDataReceived), new object[] { dataIn });
+                return;
+            }
+            // Do something with the keyboard event
+            if (dataIn.StartsWith("HEARTBEAT"))
+            {
+                // ignore heartbeat responses
+                return;
+            }
+
+
+            NativeMethods.INPUT keyEvent = new NativeMethods.INPUT();
+            int size = Marshal.SizeOf(keyEvent);
+            if (dataIn.Length != size)
+            {
+                // Error handling TBA - dont normally get TCP fragments on a LAN so not critical 
+                return;
+            }
+
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(dataIn);
+            Marshal.Copy(buffer, 0, ptr, size);
+            keyEvent = (NativeMethods.INPUT)Marshal.PtrToStructure(ptr, keyEvent.GetType());
+            Marshal.FreeHGlobal(ptr);
+            NativeMethods.SendInput(1, new NativeMethods.INPUT[] { keyEvent }, Marshal.SizeOf(keyEvent));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TCPClient TCPClient = TCPClient.Instance;
+            TCPClient.DataReceived += OnTCPDataReceived;
+            TCPClient.StatusChanged += OnTCPStatusChanged;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DontClose = false;
+            TCPClient.Instance.Close();
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            TCPClient TCPClient = TCPClient.Instance;
+            if (!TCPClient.ClientConnected)
+            {
+                TCPClient.Open();
+            }
+            if (TCPClient.ClientConnected)
+            {
+                timer1.Interval = 60000; // 60 seconds still connected test
+                lbStatus.Text = "Connected";
+                lbStatus.ForeColor = Color.Green;
+                timer1.Interval = 60000; // 60 seconds still connected test
+                TCPClient.Send("HEARTBEAT");
+            }
+            else
+            {
+                timer1.Interval = 10000; // 10 second test to try to connect
+                lbStatus.Text = "Disconnected";
+                lbStatus.ForeColor = Color.Red;
+                timer1.Interval = 10000; // 10 second test to try to connect
+            }
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                //this.WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = true;
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void bnCancel_Click(object sender, EventArgs e)
+        {
+            tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
+            tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
+            this.Hide();
+        }
+
+        private void bnSave_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ServerAddress = tbSvrAddress.Text;
+            Properties.Settings.Default.ServerPort = Convert.ToInt16(tbSvrPort.Text);
+            Properties.Settings.Default.Save();
+            TCPClient.Instance.Close();
+            lbStatus.Text = "Disconnected";
+            lbStatus.ForeColor = Color.Red;
+            TCPClient.Instance.Open();
+            timer1.Interval = 5000;
+            this.Hide();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
+            tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Visible = true;
+            this.BringToFront();
+        }
     }
-
-    private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
-    {
-      tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
-      tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
-      this.Show();
-      this.WindowState = FormWindowState.Normal;
-      this.Visible = true;
-      this.BringToFront();
-    }
-
-    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-    {
-      if ((e.CloseReason == CloseReason.UserClosing) && (DontClose))
-      {
-        e.Cancel = true;
-        this.Hide();
-      }
-    }
-
-    private void OnTCPDataReceived(string dataIn)
-    {
-      if (this.DontClose == false) { return; }
-      //Handle multi-threading
-      if (InvokeRequired)
-      {
-        Invoke(new SafeCallDelegate(OnTCPDataReceived), new object[] { dataIn });
-        return;
-      }
-      // Do something with the keyboard event
-      NativeMethods.INPUT keyEvent = new NativeMethods.INPUT();
-      int size = Marshal.SizeOf(keyEvent);
-      if (dataIn.Length != size)
-      {
-        // Error handling TBA - dont normally get TCP fragments on a LAN so not critical 
-        return;
-      }
-
-
-      IntPtr ptr = Marshal.AllocHGlobal(size);
-      byte[] buffer = System.Text.Encoding.ASCII.GetBytes(dataIn);
-      Marshal.Copy(buffer, 0, ptr, size);
-      keyEvent = (NativeMethods.INPUT)Marshal.PtrToStructure(ptr, keyEvent.GetType());
-      Marshal.FreeHGlobal(ptr);
-      NativeMethods.SendInput(1, new NativeMethods.INPUT[] { keyEvent }, Marshal.SizeOf(keyEvent));
-    }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-      TCPClient TCPClient = TCPClient.Instance;
-      TCPClient.DataReceived += OnTCPDataReceived;
-    }
-
-    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      DontClose = false;
-      this.Close();
-    }
-
-    private void timer1_Tick(object sender, EventArgs e)
-    {
-      timer1.Enabled = false;
-      TCPClient TCPClient = TCPClient.Instance;
-      if (!TCPClient.ClientConnected)
-      {
-        TCPClient.Open();
-      }
-      if (TCPClient.ClientConnected)
-      {
-        timer1.Interval = 60000; // 60 seconds still connected test
-        lbStatus.Text = "Connected";
-        lbStatus.ForeColor = Color.Green;
-      }
-      else
-      {
-        timer1.Interval = 10000; // 10 second test to try to connect
-        lbStatus.Text = "Disconnected";
-        lbStatus.ForeColor = Color.Red;
-      }
-
-      timer1.Enabled = true;
-    }
-
-    private void Form1_Resize(object sender, EventArgs e)
-    {
-      if (FormWindowState.Minimized == this.WindowState)
-      {
-        //this.WindowState = FormWindowState.Normal;
-        notifyIcon1.Visible = true;
-        this.Hide();
-      }
-      else if (FormWindowState.Normal == this.WindowState)
-      {
-        notifyIcon1.Visible = true;
-      }
-    }
-
-    private void bnCancel_Click(object sender, EventArgs e)
-    {
-      tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
-      tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
-      this.Hide();
-    }
-
-    private void bnSave_Click(object sender, EventArgs e)
-    {
-      Properties.Settings.Default.ServerAddress = tbSvrAddress.Text;
-      Properties.Settings.Default.ServerPort = Convert.ToInt16(tbSvrPort.Text);
-      Properties.Settings.Default.Save();
-      this.Hide();
-      TCPClient.Instance.Close();
-      TCPClient.Instance.Open();
-    }
-
-    private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      tbSvrAddress.Text = Properties.Settings.Default.ServerAddress;
-      tbSvrPort.Text = Properties.Settings.Default.ServerPort.ToString();
-      this.Show();
-      this.WindowState = FormWindowState.Normal;
-      this.Visible = true;
-      this.BringToFront();
-    }
-  }
 }
--- KeyPressReceiver/TCPClient.cs
+++ KeyPressReceiver/TCPClient.cs
@@ -8,212 +8,225 @@ using System.Text;
 
 namespace KeyPressReceiver
 {
-  public sealed class TCPClient
-  {
-    Socket Clientsocket;
-
-    Thread _readThread;
-    volatile bool _keepReading;
-
-    //begin Singleton pattern
-    static readonly TCPClient instance = new TCPClient();
-
-    // Explicit static constructor to tell C# compiler
-    // not to mark type as beforefieldinit
-    static TCPClient()
+    public sealed class TCPClient
     {
-    }
+        Socket Clientsocket;
 
-    TCPClient()
-    {
-      Clientsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-      _readThread = null;
-      _keepReading = false;
-    }
+        Thread _readThread;
+        volatile bool _keepReading;
 
-    public bool ClientConnected
-    {
-      get
-      {
-        if (Clientsocket != null)
-          return Clientsocket.Connected;
-        else
-          return false;
-      }
-    }
+        //begin Singleton pattern
+        static readonly TCPClient instance = new TCPClient();
 
-    public static TCPClient Instance
-    {
-      get
-      {
-        return instance;
-      }
-    }
-    //end Singleton pattern
-
-    //begin Observer pattern
-    public delegate void EventHandler(string param);
-    public EventHandler StatusChanged;
-    public EventHandler DataReceived;
-    //end Observer pattern
-
-    private void StartReading()
-    {
-      if (!_keepReading)
-      {
-        _keepReading = true;
-        _readThread = new Thread(ReadPort);
-        _readThread.Start();
-      }
-    }
-
-    private void StopReading()
-    {
-      if (_keepReading)
-      {
-        _keepReading = false;
-        if ((Clientsocket != null) && (Clientsocket.Connected)) { Clientsocket.Close(); }
-        _readThread.Join();	//block until exits
-        _readThread = null;
-      }
-    }
-
-    /// <summary> Get the data and pass it on. </summary>
-    private void ReadPort()
-    {
-      while (_keepReading)
-      {
-        if ((Clientsocket != null) && Clientsocket.Connected)
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static TCPClient()
         {
-          //Read the command's Type.
-          byte[] buffer = new byte[1024];
-          try
-          {
-            int readBytes = this.Clientsocket.Receive(buffer, buffer.Length, SocketFlags.None);
-            if (readBytes == 0)
+        }
+
+        TCPClient()
+        {
+            Clientsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _readThread = null;
+            _keepReading = false;
+        }
+
+        public bool ClientConnected
+        {
+            get
             {
-              // Socket might have closed on the remote side - just keep reading and it will close in time
-              TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
-              Thread.Sleep(waitTime);
-              continue;
+                if (Clientsocket != null)
+                    return Clientsocket.Connected;
+                else
+                    return false;
             }
-            String DataIn = System.Text.Encoding.ASCII.GetString(buffer, 0, readBytes);
-            if (DataReceived != null)
-              DataReceived(DataIn);
-          }
-          catch (SocketException)
-          {
-            // Socket closed - exit loop
-            if ((Clientsocket != null) && !Clientsocket.Connected)
+        }
+
+        public static TCPClient Instance
+        {
+            get
             {
-              IPEndPoint remoteIpEndPoint = Clientsocket.RemoteEndPoint as IPEndPoint;
-              if (remoteIpEndPoint != null)
-              {
+                return instance;
+            }
+        }
+        //end Singleton pattern
+
+        //begin Observer pattern
+        public delegate void EventHandler(string param);
+        public EventHandler StatusChanged;
+        public EventHandler DataReceived;
+        //end Observer pattern
+
+        private void StartReading()
+        {
+            if (!_keepReading)
+            {
+                _keepReading = true;
+                _readThread = new Thread(ReadPort);
+                _readThread.Start();
+            }
+        }
+
+        private void StopReading()
+        {
+            if (_keepReading)
+            {
+                _keepReading = false;
+                if ((Clientsocket != null) && (Clientsocket.Connected)) { Clientsocket.Close(); }
+                _readThread.Join(); //block until exits
+                _readThread = null;
+            }
+        }
+
+        /// <summary> Get the data and pass it on. </summary>
+        private void ReadPort()
+        {
+            while (_keepReading)
+            {
+                if ((Clientsocket != null) && Clientsocket.Connected)
+                {
+                    //Read the command's Type.
+                    byte[] buffer = new byte[1024];
+                    try
+                    {
+                        int readBytes = this.Clientsocket.Receive(buffer, buffer.Length, SocketFlags.None);
+                        if (readBytes == 0)
+                        {
+                            // Socket might have closed on the remote side - just keep reading and it will close in time
+                            TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
+                            Thread.Sleep(waitTime);
+                            continue;
+                        }
+                        String DataIn = System.Text.Encoding.ASCII.GetString(buffer, 0, readBytes);
+                        if (DataReceived != null)
+                            DataReceived(DataIn);
+                    }
+                    catch (SocketException)
+                    {
+                        // Socket closed - exit loop
+                        if ((Clientsocket != null) && !Clientsocket.Connected)
+                        {
+                            IPEndPoint remoteIpEndPoint = null;
+                            try
+                            {
+                                remoteIpEndPoint = Clientsocket.RemoteEndPoint as IPEndPoint;
+                            }
+                            catch
+                            {
+                                remoteIpEndPoint = null;
+                            }
+
+                            if (remoteIpEndPoint != null)
+                            {
+                                if (StatusChanged != null)
+                                    StatusChanged(String.Format("Client disconnected from:{0}", remoteIpEndPoint.Address));
+                            }
+                            Clientsocket.Dispose();
+                            Clientsocket = null;
+                        }
+                        continue;
+                    }
+                }
+                else
+                {
+                    // Socket closed - exit loop
+                    if ((Clientsocket != null) && !Clientsocket.Connected)
+                    {
+                        IPEndPoint remoteIpEndPoint = Clientsocket.RemoteEndPoint as IPEndPoint;
+                        if (remoteIpEndPoint != null)
+                        {
+                            if (StatusChanged != null)
+                                StatusChanged(String.Format("Client disconnected from:{0}", remoteIpEndPoint.Address));
+                        }
+                        Clientsocket.Dispose();
+                        Clientsocket = null;
+                    }
+                    TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
+                    Thread.Sleep(waitTime);
+                }
+            }
+        }
+
+        /// <summary> Open the serial port with current settings. </summary>
+        public void Open()
+        {
+            Close();
+
+            try
+            {
+                if (Clientsocket == null)
+                    Clientsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Clientsocket.Connect(Properties.Settings.Default.ServerAddress, Properties.Settings.Default.ServerPort);
+                StartReading();
+            }
+            catch (IOException)
+            {
                 if (StatusChanged != null)
-                  StatusChanged(String.Format("Client disconnected from:{0}", remoteIpEndPoint.Address));
-              }
-              Clientsocket.Dispose();
-              Clientsocket = null;
+                    StatusChanged(String.Format("{0} does not exist", Properties.Settings.Default.ServerAddress));
             }
-            continue;
-          }
-        }
-        else
-        {
-          // Socket closed - exit loop
-          if ((Clientsocket != null) && !Clientsocket.Connected)
-          {
-            IPEndPoint remoteIpEndPoint = Clientsocket.RemoteEndPoint as IPEndPoint;
-            if (remoteIpEndPoint != null)
+            catch (UnauthorizedAccessException)
             {
-              if (StatusChanged != null)
-                StatusChanged(String.Format("Client disconnected from:{0}", remoteIpEndPoint.Address));
+                if (StatusChanged != null)
+                    StatusChanged(String.Format("{0} already in use", Properties.Settings.Default.ServerPort));
             }
-            Clientsocket.Dispose();
-            Clientsocket = null;
-          }
-          TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
-          Thread.Sleep(waitTime);
+            catch (Exception ex)
+            {
+                if (StatusChanged != null)
+                    StatusChanged(String.Format("{0}", ex.ToString()));
+                Clientsocket.Dispose();
+                Clientsocket = null;
+            }
         }
-      }
-    }
 
-    /// <summary> Open the serial port with current settings. </summary>
-    public void Open()
-    {
-      Close();
-
-      try
-      {
-        Clientsocket.Connect(Properties.Settings.Default.ServerAddress, Properties.Settings.Default.ServerPort);
-        StartReading();
-      }
-      catch (IOException)
-      {
-        if (StatusChanged != null)
-          StatusChanged(String.Format("{0} does not exist", Properties.Settings.Default.ServerAddress));
-      }
-      catch (UnauthorizedAccessException)
-      {
-        if (StatusChanged != null)
-          StatusChanged(String.Format("{0} already in use", Properties.Settings.Default.ServerPort));
-      }
-      catch (Exception ex)
-      {
-        if (StatusChanged != null)
-          StatusChanged(String.Format("{0}", ex.ToString()));
-      }
-    }
-
-    /// <summary> Close the serial port. </summary>
-    public void Close()
-    {
-      StopReading();
-      if ((Clientsocket != null) && (Clientsocket.Connected)) { Clientsocket.Close(); }
-      // Svrsocket.Close();
-      if (StatusChanged != null)
-        StatusChanged("connection closed");
-    }
-
-    /// <summary> Get the status of the serial port. </summary>
-    public bool IsOpen
-    {
-      get
-      {
-        return Clientsocket.Connected;
-      }
-    }
-
-    /// <summary>Send data to the serial port after appending line ending. </summary>
-    /// <param name="data">An string containing the data to send. </param>
-    //This Semaphor is to protect the critical section from concurrent access of sender threads.
-    System.Threading.Semaphore semaphor = new System.Threading.Semaphore(1, 1);
-    public Boolean Send(string data)
-    {
-      try
-      {
-        semaphor.WaitOne();
-        //Type
-        byte[] buffer = new byte[data.Length];
-        buffer = Encoding.ASCII.GetBytes(data);
-        if ((Clientsocket != null) && Clientsocket.Connected)
+        /// <summary> Close the serial port. </summary>
+        public void Close()
         {
-          int BytesSent = Clientsocket.Send(buffer, data.Length, SocketFlags.None);
-          semaphor.Release();
+            StopReading();
+            if ((Clientsocket != null) && (Clientsocket.Connected)) { Clientsocket.Close(); }
+            // Svrsocket.Close();
+            if (StatusChanged != null)
+                StatusChanged("connection closed");
+        }
 
-          return (Clientsocket.Connected && (BytesSent == data.Length));
-        }
-        else
+        /// <summary> Get the status of the serial port. </summary>
+        public bool IsOpen
         {
-          semaphor.Release();
-          return false;
+            get
+            {
+                return Clientsocket.Connected;
+            }
         }
-      }
-      catch
-      {
-        semaphor.Release();
-        return false;
-      }
+
+        /// <summary>Send data to the serial port after appending line ending. </summary>
+        /// <param name="data">An string containing the data to send. </param>
+        //This Semaphor is to protect the critical section from concurrent access of sender threads.
+        System.Threading.Semaphore semaphor = new System.Threading.Semaphore(1, 1);
+        public Boolean Send(string data)
+        {
+            try
+            {
+                semaphor.WaitOne();
+                //Type
+                byte[] buffer = new byte[data.Length];
+                buffer = Encoding.ASCII.GetBytes(data);
+                if ((Clientsocket != null) && Clientsocket.Connected)
+                {
+                    int BytesSent = Clientsocket.Send(buffer, data.Length, SocketFlags.None);
+                    semaphor.Release();
+
+                    return (Clientsocket.Connected && (BytesSent == data.Length));
+                }
+                else
+                {
+                    semaphor.Release();
+                    return false;
+                }
+            }
+            catch
+            {
+                semaphor.Release();
+                return false;
+            }
+        }
     }
-  }
 }
