Gadroc/helios

commit e2bda12e85f1e06124f17a387f8438a7069a71ee

eliminated use of exception for control flow

Helios/Controls/RotarySwitch.cs
@@ -634,20 +634,15 @@ namespace GadrocsWorkshop.Helios.Controls
 
         void SetPositionAction_Execute(object action, HeliosActionEventArgs e)
         {
-            try
+            BeginTriggerBypass(e.BypassCascadingTriggers);
+            if (int.TryParse(e.Value.StringValue, out int index))
             {
-                BeginTriggerBypass(e.BypassCascadingTriggers);
-                int index = int.Parse(e.Value.StringValue);
-                if (index > 0 && index <= Positions.Count)
+                if (index >= 0 && index < Positions.Count)
                 {
                     CurrentPosition = index;
                 }
-                EndTriggerBypass(e.BypassCascadingTriggers);
             }
-            catch
-            {
-                // No-op if the parse fails we won't set the position.
-            }
+            EndTriggerBypass(e.BypassCascadingTriggers);
         }
 
         #endregion

Helios/Controls/KneeBoard.cs
@@ -219,20 +219,15 @@ namespace GadrocsWorkshop.Helios.Controls
 
         void SetPositionAction_Execute(object action, HeliosActionEventArgs e)
         {
-            try
+            BeginTriggerBypass(e.BypassCascadingTriggers);
+            if (int.TryParse(e.Value.StringValue, out int index))
             {
-                BeginTriggerBypass(e.BypassCascadingTriggers);
-                int index = int.Parse(e.Value.StringValue);
                 if (index >= 0 && index < Positions.Count)
                 {
                     CurrentPosition = index;
                 }
-                EndTriggerBypass(e.BypassCascadingTriggers);
             }
-            catch
-            {
-                // No-op if the parse fails we won't set the position.
-            }
+            EndTriggerBypass(e.BypassCascadingTriggers);
         }
 
         #endregion

Helios/HeliosBinding.cs
@@ -428,9 +428,8 @@ namespace GadrocsWorkshop.Helios
                             case BindingValueSources.LuaScript:
                                 try
                                 {
-
                                     object[] returnValues = LuaInterpreter.DoString(Value);
-                                    if (returnValues.Length >= 1)
+                                    if ((returnValues != null) && (returnValues.Length >= 1))
                                     {
                                         value = CreateBindingValue(returnValues[0]);
                                         if (ConfigManager.LogManager.LogLevel >= LogLevel.Debug)
@@ -449,6 +448,7 @@ namespace GadrocsWorkshop.Helios
                                 }
                                 catch (Exception valueException)
                                 {
+                                    // these are exceptions thrown by the Lua implementation
                                     ConfigManager.LogManager.LogError("Binding value lua script has thown an unhandled exception. (Binding=\"" + Description + "\", Value Script=\"" + Value + "\")", valueException);
                                 }
                                 break;

commit 21ae301308ff0128f3a5333a1fa6f914fdae84c8

fixed rotaryswitch broken in Q1 2020 release
switch could no longer be externally set to its last position via binding
bug was introduced in e2bda12e85f1e06124f17a387f8438a7069a71ee and shipped in Q1 2020 release

Helios/Controls/RotarySwitch.cs
@@ -637,7 +637,8 @@ namespace GadrocsWorkshop.Helios.Controls
             BeginTriggerBypass(e.BypassCascadingTriggers);
             if (int.TryParse(e.Value.StringValue, out int index))
             {
-                if (index >= 0 && index < Positions.Count)
+                // WARNING: rotary switch positions are 1-based
+                if (index > 0 && index <= Positions.Count)
                 {
                     CurrentPosition = index;
                 }